20d1c94c61674753ff2105e5451a7f27b7e8f5f6
HB-Stratos <56313836+HB-Stratos@users.noreply.github.com>
2024-08-15 18:55:29 +0200

    Some experimentation

---
+++ KspTestMod1/KspTestMod.cs
@@ -0,0 +1,35 @@
+﻿using DebugStuff;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+
+namespace KspTestMod1
+{
+    [KSPAddon(KSPAddon.Startup.Flight, false)]
+    public class KspTestMod : MonoBehaviour
+    {
+        public GameObject[] cubes;
+        public void Update()
+        {
+            bool key = Input.GetKey(KeyCode.LeftAlt) && Input.GetKeyDown(KeyCode.Alpha1);
+            if (key)
+            {
+                List<Part> parts = FlightGlobals.ActiveVessel.parts;
+                int index;
+                System.Random rnd = new System.Random();
+                index = rnd.Next(1, parts.Count); //we ignore the root part by starting at 1
+                //parts[index].explode();
+                DebugDrawer.DebugLine(parts[0].transform.position, parts[index].transform.position, Color.red);
+                cubes.Append(GameObject.CreatePrimitive(PrimitiveType.Cube));
+                cubes.Last().transform.position = parts[0].transform.position;
+                BoxCollider cubeCollider = cubes.Last().GetComponent<BoxCollider>();
+                cubeCollider.enabled = false;
+            }
+            foreach (GameObject cube in cubes)
+            {
+                //cube.transform.position =
+            }
+        }
+    }
+}
